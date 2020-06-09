44433b4595476eb175ea7fc8c62748c1160ce019
Kyle Yates <53956594+KyleTy1er@users.noreply.github.com>
2020-06-09 11:06:25 -0700

    finished queue stack assignment

--- stack/stack.py
+++ stack/stack.py
@@ -9,17 +9,53 @@ return elements in Last In First Out order.
    Make sure the Stack tests pass.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
+
+   When using an array we can use append and pop, but with the Linked List
+   we have to follow the Class constraints of head and tail values.
+
 """
+
+import sys
+
+sys.path.append('../singly_linked_list')
+from singly_linked_list import LinkedList
+
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
-
-    def __len__(self):
-        pass
+        self.storage = []
 
     def push(self, value):
-        pass
+        self.size = len(self.storage)
+        self.storage.append(value)
 
     def pop(self):
-        pass
+        if self.size == 0:
+            return None
+        else:
+            return self.storage.pop()
+
+    def len(self):
+        self.size = len(self.storage)
+        return self.size
+
+
+
+class Stack:
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+
+    def push(self, value):
+        self.size += 1
+        self.storage.add_to_tail(value)
+
+    def pop(self):
+        if self.size == 0:
+            return None
+        self.size -= 1
+        return self.storage.remove_tail()
+
+    def len(self):
+        return self.size
